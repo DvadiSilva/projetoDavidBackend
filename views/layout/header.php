d188374763cea2a31fe2ee43dbfd8d2f7054d638
Dvadi <david.s.vieira15@gmail.com>
2022-10-02 23:53:10 +0100

    layout da página de erro

--- views/layout/header.php
+++ views/layout/header.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title ?> - Jornalinho</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="/css/base.css">
     </head>
     <body>
         <nav class="navbar navbar-expand-lg bg-light">
