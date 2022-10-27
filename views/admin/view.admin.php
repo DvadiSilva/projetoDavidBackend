d608ba5e23e0361a40b76548fe067c7ca1a0005f
Dvadi <david.s.vieira15@gmail.com>
2022-10-27 01:38:55 +0100

    Correção de layout

--- views/admin/view.admin.php
+++ views/admin/view.admin.php
@@ -3,9 +3,12 @@
     require("view.navBar.php");
 ?>
 
-    <div class="col-9">
-        <h2>Painel de controlo</h2>
-        <h3>Bem-vindo a sua página de administração</h3>
+    <div class="col-11 d-flex justify-content-between align-items-center">
+        <div>
+            <h2>Painel de controlo</h2>
+            <h3>Bem-vindo a sua página de administração</h3>
+        </div>
+        <img src="/images/jornalinho.png" alt="" class="">
     </div>
 </section>
 
