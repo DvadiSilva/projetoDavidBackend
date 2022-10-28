4ce8d64796988e30b588fa7f2f528ac1ee95a0f0
Dvadi <david.s.vieira15@gmail.com>
2022-10-28 19:57:14 +0100

    Correção de detalhes

--- views/admin/view.users.php
+++ views/admin/view.users.php
@@ -7,7 +7,7 @@
         <div class="d-flex justify-content-between align-items-center">
             <h2>Utilizadores</h2>
             <form class="d-flex align-items-center" action="/admin/users" method="post">
-                <input type="text" name="searchUser" aria-label="search user" placeholder="Pesquisar Utilizador" minlength="1" maxlength="30">
+                <input type="text" name="searchUser" aria-label="search user" placeholder="Pesquisar Utilizador" minlength="1" maxlength="30" required>
                 <button type="submit" class="btn btn-primary" name="send">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
